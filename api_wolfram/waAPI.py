39dd9e959585cb8f658ce149718fefe568115561
Hamza <hamza.27.96@icloud.com>
2020-03-30 18:03:18 +0200

    Added extraction of results, limits, partial derivatives and integrals in Expression class. Added success field in Expression class. Added function to convert and save plots. Added function raw. Added some examples in latex

--- api_wolfram/waAPI.py
+++ api_wolfram/waAPI.py
@@ -4,15 +4,34 @@
 
 import requests
 import urllib.parse
-import json
+import urllib.request
 import base64
 import xmltodict
-import urllib.request
 import pickle
+import os.path
+from PIL import Image
+from io import BytesIO
 
 API_URL = 'https://api.wolframalpha.com/v2/query'
 API_SIGNUP_PAGE = 'https://developer.wolframalpha.com'
 KEY = 'V2WJ46-EEXEV95WXG'
+
+
+def raw(text):
+    """
+    Returns a raw string representation of text
+    """
+    escape_dict = {'\a': '\\a',
+                   '\b': '\\b',
+                   '\f': '\\f',
+                   '\n': '\\n',
+                   '\r': '\\r',
+                   '\t': '\\t',
+                   '\v': '\\v'}
+    for k, v in escape_dict.items():
+        text = text.replace(k, v)
+
+    return text
 
 
 class NoAPIKeyException(Exception):
@@ -31,10 +50,9 @@ class waAPI(object):
         Request a key at https://developer.wolframalpha.com
 
         :param key: Wolfram Alpha App ID Developer Key
-
         """
         self.key = key
-        self.response_format = 'json'
+        self.response_format = 'xml'
 
         if self.key is None:
             raise NoAPIKeyException(
@@ -51,12 +69,10 @@ class waAPI(object):
 
         :param response_format: the format that the API uses for its response,
                                 includes JSON (.json) and XML.
-                                Defaults to '.json'.
+                                Defaults to '.xml'.
 
         :param key: a developer key. Defaults to key given when the waAPI class was initialized.
 
-        :**kwargs examples:
-                        query = 'How many megabytes are in a gigabyte'
         """
         if key is None:
             key = self.key
@@ -65,89 +81,151 @@ class waAPI(object):
         if query is None:
             return 'Sorry, I did not get your question.'
         else:
+            query = raw(query)
             url = '%s?input=%s&podstate%s&output=%s&appid=%s' % (API_URL, urllib.parse.quote(
-                query), 'Result__Step-by-step+solution', 'xml', key)
-
+                query), 'Result__Step-by-step+solution', self.response_format, key)
             r = urllib.request.urlopen(url)
             result = xmltodict.parse(r, dict_constructor=dict)['queryresult']
             return result
 
 
 class Expression(object):
-    def __init__(self, query, results):
+    def __init__(
+            self,
+            query,
+            results,
+            id_equation=None,
+            dir_plots='plot_images'):
         """
         Initializes the Expression class with a results from Wolfram Alpha API.
 
         Request a query.
         Request a JSON results.
 
-        :param results: Results of query from Wolfram Alpha App
-        :param results: Results of query from Wolfram Alpha App
-
+        :param query: expression query
+        :param results: results of query from Wolfram Alpha App
+        :param dir_plots: rir where to save plots
+        :param id_equation: identifier of expression
         """
 
         self.query = query
-        self.execution_time = None
+        self.success = results['@success']
+        self.execution_time = results['@timing']
         self.plots = []
         self.alternate_forms = []
         self.solutions = []
-        #self.steps = []
+        self.results = []
+        self.limits = []
+        self.partial_derivatives = []
+        self.integral = []
 
-        self.execution_time = results['@timing']
+        if self.success == 'true':
+            for pod in results['pod']:
+                try:
+                    print(pod['@id'])
+                    if pod['@id'].find("Plot") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                src_plot = subpod['img']['@src']
+                                self.plots.append(
+                                    base64.b64encode(
+                                        requests.get(src_plot).content))
+                        else:
+                            src_plot = subpod['img']['@src']
+                            self.plots.append(src_plot)
 
-        for pod in results['pod']:
-            print(pod['@id'])
-            if pod['@id'].find("Plot") >= 0:
-                if isinstance(pod['subpod'], list):
-                    for subpod in pod['subpod']:
-                        src_plot = subpod['img']['@src']
-                        self.plots.append(
-                            base64.b64encode(
-                                requests.get(src_plot).content))
-                else:
-                    src_plot = pod['subpod']['img']['@src']
-                    self.plots.append(
-                        base64.b64encode(
-                            requests.get(src_plot).content))
+                    if pod['@id'].find("AlternateForm") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.alternate_forms.append(
+                                    subpod['plaintext'])
+                        else:
+                            self.alternate_forms.append(
+                                pod['subpod']['plaintext'])
 
-            if pod['@id'].find("AlternateForm") >= 0:
-                if isinstance(pod['subpod'], list):
-                    for subpod in pod['subpod']:
-                        self.alternate_forms.append(subpod['plaintext'])
-                else:
-                    self.alternate_forms.append(pod['subpod']['plaintext'])
+                    if pod['@id'].find("Solution") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.solutions.append(subpod['plaintext'])
+                        else:
+                            self.solutions.append(pod['subpod']['plaintext'])
 
-            if pod['@id'].find("Solution") >= 0:
-                if isinstance(pod['subpod'], list):
-                    for subpod in pod['subpod']:
-                        self.solutions.append(subpod['plaintext'])
-                else:
-                    self.solutions.append(pod['subpod']['plaintext'])
+                    if pod['@id'].find("Result") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.results.append(subpod['plaintext'])
+                        else:
+                            self.results.append(pod['subpod']['plaintext'])
+
+                    if pod['@id'].find("Limit") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.limits.append(subpod['plaintext'])
+                        else:
+                            self.limits.append(pod['subpod']['plaintext'])
+
+                    if pod['@id'].find("Derivative") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.partial_derivatives.append(
+                                    subpod['plaintext'])
+                        else:
+                            self.partial_derivatives.append(
+                                pod['subpod']['plaintext'])
+
+                    if pod['@id'].find("Integral") >= 0:
+                        if isinstance(pod['subpod'], list):
+                            for subpod in pod['subpod']:
+                                self.integral.append(subpod['plaintext'])
+                        else:
+                            self.integral.append(pod['subpod']['plaintext'])
+
+                except BaseException:
+                    print("Error in extraction:", pod['@id'])
+
+        if id_equation is not None:
+            self.save_plots(id_equation, dir_plots)
+
+    def save_plots(self, id_equation, dir_plots):
+        """
+        Save image plots in dir_plots
+        """
+        for i in range(len(self.plots)):
+            filename_img = str(id_equation) + '_' + str(i) + '.png'
+            path_img = os.path.join(dir_plots, filename_img)
+
+            im = Image.open(BytesIO(base64.b64decode(self.plots[i])))
+            im.save(path_img, 'PNG')
+
+            self.plots[i] = path_img
 
     def print_expression(self):
         """
         Print content of the expression.
-
         """
-        print("\nExpression info")
-        print("Query: ", self.query)
+        print("\nExpression information")
+        print("Success: ", self.success)
+        print("Query: ", raw(self.query))
         print("Execution time: ", self.execution_time)
-        print("Plots (base64): ", len(self.plots))
+        print("Plots: ", self.plots)
         print("Alternate forms: ", self.alternate_forms)
+        print("Results: ", self.results)
         print("Solutions: ", self.solutions)
+        print("Limit: ", self.limits)
+        print("Partial derivatives: ", self.partial_derivatives)
+        print("Integral: ", self.integral)
 
 
 if __name__ == "__main__":
-    '''
+    """
     Query examples:
-        int e^x^2 dx
+        \int x^2 dx
         x^3 + x^2 y + x y^2 + y^3
-        (x^2-1)/(x^2+1)
-        cos(arcsin(x)/2)
-        
-    '''
-    query = 'cos(arcsin(x)/2)'
-
+        \frac{x^2-1}{x^2+1}
+        \cos{\frac{\arcsin{x}}{2}}
+        2x+17y=23,x-y=5,\int_{0}^{x} x dx
+    """
+    query = '\cos{\frac{\arcsin{x}}{2}}'
     api = waAPI(KEY)
     results = api.full_results(query=query)
 
@@ -159,5 +237,7 @@ if __name__ == "__main__":
     # results = pickle.load(file)
     # file.close()
 
-    obj = Expression(query, results)
+    id_equation = '001'
+
+    obj = Expression(query=query, results=results, id_equation=id_equation)
     obj.print_expression()
