ad8e910fa73a3207941ff75e1eb96427805eb44f
Hamza <hamza.27.96@icloud.com>
2020-04-14 22:38:40 +0200

    Fixing deploy

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -24,7 +24,8 @@ def create_app(config_name):
     logging.info("Upload folder = " + os.path.join(os.path.abspath(os.path.dirname(__file__)), '/usr/src/sciq/web/app/static/uploads'))
 
     db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     ma.init_app(app)
     heroku.init_app(app)
 
