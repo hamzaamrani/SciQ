58efd53e04703fe6bc4f5a2a37161bc6e0f07aef
Samuele Ventura <s.ventura6@campus.unimib.it>
2020-04-24 13:09:57 +0000

    Update run_prod.py [skip-ci]

--- web/run_prod.py
+++ web/run_prod.py
@@ -1,22 +1,11 @@
-#from flask_migrate import MigrateCommand
-#from flask_script import Manager
-
 from web.app import create_app
 
 from os import environ
-
-# to del
-print("Valore env var STEP: ", environ.get('STEP'))
 
 if environ.get('STEP') == 'staging':
     app = create_app("pre_prod")
 else: 
     app = create_app("production")
-
-print("URI database: ", app.config['SQLALCHEMY_DATABASE_URI'])
-
-#manager = Manager(app)
-#manager.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
