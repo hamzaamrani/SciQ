f70eb5c79454ef95906610183f73b7de2ba2c083
Costantino <ccarta@systemasrl.it>
2020-04-25 14:29:52 +0200

    fix post request

--- web/app/static/js/requests.js
+++ web/app/static/js/requests.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
 
     $("#login_form").on("submit", function(event){
-
         $.ajax({
             data: {
                 username : $('#username_login').val(),
                 password : $('#password_login').val()
             },
-            type : 'POST',
-            url : '/login'
-        }).done(function(data){
-            console.log(data)
-            if(data.error){
-                $('#error_alert').text(data.error).show();
-                $('#success_alert').hide();
-            }else{
-                $('#success_alert').text(data.results).show();
-                $('#error_alert').hide();
+            method : "POST",
+            url : '/login',
+            success: function(data) {
+                if(data.error){
+                    $('#error_alert').text(data.error).show();
+                    $('#success_alert').hide();
+                }else{
+                    $('#success_alert').text(data.results).show();
+                    $('#error_alert').hide();  
+                }
                 window.location.href = "logged_user.html";
             }
-        })
-
+        });
         event.preventDefault();
     });
-
 });
