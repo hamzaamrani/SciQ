3e0e65b012d2fd01e35e6d92a7fe4a85a25b2daa
Costantino <ccarta@systemasrl.it>
2020-05-29 15:06:41 +0200

    Fix collection, Modify fragment

--- web/app/api/collections_api.py
+++ web/app/api/collections_api.py
@@ -38,6 +38,7 @@ def create_collection_json(collections_names, collections_infos,expressions_by_c
     for name, info, expressions in zip(collections_names, collections_infos,expressions_by_collection):
         expressions_json=[]
         for expression in expressions:
+            expression['_id'] = str(expression['_id'])
             expressions_json.append( {k: v for k, v in expression.__dict__.items()} )
 
         collections.append({'name':name, 'info':info, 'expressions':expressions_json})
