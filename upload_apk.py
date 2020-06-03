cf56d34961dd409eb4d17b217e0963db907246c1
belerico <belo.fede@outlook.com>
2020-06-03 23:10:43 +0200

    FIx name

--- upload_apk.py
+++ upload_apk.py
@@ -17,7 +17,7 @@ def upload(file, apks_dir, oauth2_access_token):
     )
     os.rename(
         os.path.join(apks_dir, file),
-        os.path.join(apks_dir, "sciq_v." + max_version + ".apk"),
+        os.path.join(apks_dir, "sciq_v." + new_version + ".apk"),
     )
     dbx = dropbox.Dropbox(oauth2_access_token=oauth2_access_token)
     dbx.files_upload(open(new_version, "rb").read(), "/" + new_version)
